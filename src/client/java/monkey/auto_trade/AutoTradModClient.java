8f13f3e8ce64b1a4e51b1bbfdc140367c0b1e0f5
a3510377 <a102009102009@gmail.com>
2023-08-11 09:22:42 +0800

    feat check chunk data staus

--- src/client/java/monkey/auto_trade/AutoTradModClient.java
+++ src/client/java/monkey/auto_trade/AutoTradModClient.java
@@ -4,6 +4,7 @@ import fi.dy.masa.itemscroller.util.InventoryUtils;
 import fi.dy.masa.itemscroller.villager.VillagerDataStorage;
 import fi.dy.masa.malilib.util.GuiUtils;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
+import monkey.auto_trade.chunkdebug.ChunkData;
 import monkey.auto_trade.chunkdebug.ChunkdebugApi;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
@@ -13,22 +14,22 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.MerchantScreen;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.entity.passive.WanderingTraderEntity;
 import net.minecraft.item.Item;
 import net.minecraft.screen.MerchantScreenHandler;
 import net.minecraft.screen.slot.Slot;
+import net.minecraft.server.world.ChunkLevelType;
 import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
-import net.minecraft.util.math.Vec3d;
 
+import static monkey.auto_trade.AutoTradMod.LOGGER;
 import static net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents.START_CLIENT_TICK;
 
 public class AutoTradModClient implements ClientModInitializer {
     public static ChunkdebugApi CHUNK_DEBUG = new ChunkdebugApi();
-    public static Vec3d villagerOldPos;
-    public static Identifier villageOldWorld;
+    public static Entity tradeEntity;
     public static boolean illimitedTradeToggle;
     public static boolean startTrade;
 
@@ -43,42 +44,49 @@ public class AutoTradModClient implements ClientModInitializer {
 
             Screen screen = GuiUtils.getCurrentScreen();
             if (!startTrade) CHUNK_DEBUG.requestChunkData();
-            if (startTrade && screen instanceof MerchantScreen merchantScreen) {
+            else if (screen instanceof MerchantScreen merchantScreen) {
                 MerchantScreenHandler handler = merchantScreen.getScreenHandler();
                 IntArrayList favorites = VillagerDataStorage.getInstance().getFavoritesForCurrentVillager(handler).favorites;
 
                 if (client.player != null && !client.player.isSneaking() && !favorites.isEmpty()) {
-                    InventoryUtils.villagerTradeEverythingPossibleWithAllFavoritedTrades();
-                    for (int index = 0; index < favorites.size(); ++index) {
-                        Item sellItem = handler.getRecipes().get(index).getSellItem().getItem();
+                    Identifier world = tradeEntity.getWorld().getRegistryKey().getValue();
+                    ChunkData data = ChunkData.serverWorldChunks.get(world).get(tradeEntity.getChunkPos());
+                    LOGGER.info(String.valueOf(data.levelType()));
+                    if (data.levelType() == ChunkLevelType.INACCESSIBLE) {
+                        InventoryUtils.villagerTradeEverythingPossibleWithAllFavoritedTrades();
 
-                        // drop all sell item
-                        for (Slot slot : handler.slots) {
-                            if (slot.getStack().getItem().equals(sellItem)) {
-                                InventoryUtils.dropStack(merchantScreen, slot.id);
+                        for (int index = 0; index < favorites.size(); ++index) {
+                            Item sellItem = handler.getRecipes().get(index).getSellItem().getItem();
+
+                            // drop all sell item
+                            for (Slot slot : handler.slots) {
+                                if (slot.getStack().getItem().equals(sellItem)) {
+                                    InventoryUtils.dropStack(merchantScreen, slot.id);
+                                }
                             }
                         }
+                        merchantScreen.close();
                     }
-                    merchantScreen.close();
                 }
-            } else if (client.crosshairTarget instanceof EntityHitResult hitResult) {
+            }
+
+            if (client.crosshairTarget instanceof EntityHitResult hitResult) {
                 Entity entity = hitResult.getEntity();
-                if (entity instanceof VillagerEntity) {
-                    villagerOldPos = entity.getPos();
-                    villageOldWorld = entity.getWorld().getRegistryKey().getValue();
+                if (entity instanceof VillagerEntity || entity instanceof WanderingTraderEntity) {
+                    tradeEntity = entity;
+                    Identifier world = entity.getWorld().getRegistryKey().getValue();
 
                     if (client.interactionManager != null && client.player != null && screen == null) {
-                        if (client.player.getPos().isInRange(villagerOldPos, 2)) {
+                        if (client.player.getPos().isInRange(tradeEntity.getPos(), 2)) {
                             client.interactionManager.interactEntity(client.player, entity, Hand.MAIN_HAND);
                         }
                     }
 
                     // if in listening world != villageOldWorld re-listen
-                    if (CHUNK_DEBUG.listen == null || !CHUNK_DEBUG.listen.equals(villageOldWorld)) {
-                        CHUNK_DEBUG.requestChunkData(villageOldWorld);
+                    if (CHUNK_DEBUG.listen == null || !CHUNK_DEBUG.listen.equals(world)) {
+                        CHUNK_DEBUG.requestChunkData(world);
                     }
                 }
-            } else if (client.crosshairTarget instanceof BlockHitResult hitResult) {
             }
         });
 
--- src/client/java/monkey/auto_trade/chunkdebug/ChunkData.java
+++ src/client/java/monkey/auto_trade/chunkdebug/ChunkData.java
@@ -1,13 +1,16 @@
 package monkey.auto_trade.chunkdebug;
 
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.server.world.ChunkLevelType;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.ChunkPos;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public record ChunkData(ChunkPos chunkPos, ChunkLevelType levelType, byte statusCode, byte ticketCode) {
-    public static final Map<String, Map<ChunkPos, ChunkData>> serverWorldChunks = new HashMap<>();
+    public static final Map<Identifier, Map<ChunkPos, ChunkData>> serverWorldChunks = new HashMap<>();
 
     public static void deserialize(String world, long[] chunkPositions, byte[] levelTypes, byte[] statusTypes, byte[] ticketTypes) {
         int size = chunkPositions.length;
@@ -16,7 +19,8 @@ public record ChunkData(ChunkPos chunkPos, ChunkLevelType levelType, byte status
             return;
         }
 
-        Map<ChunkPos, ChunkData> chunkDataMap = serverWorldChunks.getOrDefault(world, new HashMap<>());
+        Identifier worldRegistryKey = RegistryKey.of(RegistryKeys.WORLD, new Identifier(world)).getValue();
+        Map<ChunkPos, ChunkData> chunkDataMap = serverWorldChunks.getOrDefault(worldRegistryKey, new HashMap<>());
         for (int i = 0; i < size; i++) {
             ChunkPos chunkPos = new ChunkPos(chunkPositions[i]);
             chunkDataMap.put(chunkPos, new ChunkData(
@@ -27,7 +31,7 @@ public record ChunkData(ChunkPos chunkPos, ChunkLevelType levelType, byte status
             ));
         }
 
-        serverWorldChunks.put(world, chunkDataMap);
+        serverWorldChunks.put(worldRegistryKey, chunkDataMap);
     }
 
     public int getPosX() {
